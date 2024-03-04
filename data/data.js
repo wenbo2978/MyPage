adbe2e828d2abd68b77c01ab41430d5e77e2b46d
wenbo2978 <161510538+wenbo2978@users.noreply.github.com>
2024-03-04 14:32:07 -0500

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,36 @@
+export const homeInformation = {
+  img: 'my2.jpg',
+  name: '',
+  title: 'Hi, This is Wenbo Chen, welcome to my page.',
+  content: ''
+};
+
+export const aboutMe = {
+  title: 'About me',
+  content: ''
+};
+
+export const myProjects = {
+  title: 'See My Latest Works.',
+  myWork: [{
+              pageLink: '#',
+              img: 'imageProcessing.jpg'
+            },
+            {
+              pageLink: '#',
+              img: 'webDevelopment.jpg'
+            },
+            {
+              pageLink: '#',
+              img: 'mobileAppDevelopment.jpg'
+            },
+            {
+              pageLink: 'https://github.com/wenbo2978',
+              img: 'myGithub.jpg'
+            },
+            {
+              pageLink: '#',
+              img: 'more.jpg'
+            }
+          ]
+};
